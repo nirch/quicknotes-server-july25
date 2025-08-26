e0a6aed7b57f92950bf807c65fb139e346e721cf
nirch <nirh2@yahoo.com>
2025-08-26 11:55:38 +0300

    serving static file in public folder

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-const notesRouter = require("./routes/noteRouter.js")
+const notesRouter = require("./routes/noteRouter.js");
 
 // Middleware that parses JSON for every route
 app.use(express.json());
+app.use(express.static("public"));
 
 // Routes
+app.use("/notes", notesRouter);
+
+// Running the server
+app.listen(PORT, () => {
+  console.log("Server is listening on port " + PORT);
+});
 
 // Home Route
 app.get("/", (req, res) => {
   res.send("Hello Express!");
 });
-
-app.use("/notes", notesRouter)
 
 // Demo Route
 app.get("/demo", (req, res) => {
@@ -24,9 +29,4 @@ app.get("/demo", (req, res) => {
   console.log("Params:", req.params);
   console.log("Body:", req.body); // Needs body parser
   res.send("Check console for request data");
-});
-
-// Running the server
-app.listen(PORT, () => {
-  console.log("Server is listening on port " + PORT);
 });
