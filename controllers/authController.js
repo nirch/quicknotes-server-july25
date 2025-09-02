4d78ee6a6a1e55f6c287d156d1a794dad5a62b5d
nirch <nirh2@yahoo.com>
2025-09-02 10:19:32 +0300

    using bcrypt to compare passwords

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,15 +4,15 @@ async function login(req, res, next) {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      throw { status: 400, message: "email or password missing" };
+      next({ status: 400, message: "email or password missing" });
     }
     const user = await authModel.login(email, password);
     if (!user) {
-      throw { status: 401, message: "invalid email or password" };
+      next({ status: 401, message: "invalid email or password" });
     }
     res.status(200).send(user);
   } catch (err) {
-    throw { status: 401, message: err };
+    next({ status: 401, message: err });
   }
 }
 
--- models/authModel.js
+++ models/authModel.js
@@ -1,26 +1,29 @@
+const bcrypt = require("bcrypt");
+
 const users = [
   {
     id: "1",
     name: "Nir Channes",
     email: "nirch@example.com",
-    password: "123",
+    password: "$2a$10$46zfiyv9XAtAB3Fh7AaMDuKd5gsRlboX8q3O4ygx/yJk4tiyCN9jK",
   },
   {
     id: "2",
     name: "John Doe",
     email: "johnd@example.com",
-    password: "123",
+    password: "$2a$10$uEp5ZfDp60ahwJwpvGyBc.D2SLTy.ssl5IylOBPZpqRWp5lQvyG5O",
   },
 ];
 
 async function login(email, password) {
   const user = users.find(
-    (user) =>
-      user.email.toLowerCase() === email.toLowerCase() &&
-      user.password === password
+    (user) => user.email.toLowerCase() === email.toLowerCase()
   );
-  const { password:_, ...userNoPassword } = user;
-  return userNoPassword;
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const { password: _, ...userNoPassword } = user;
+    return userNoPassword;
+  }
 }
 
 module.exports = { login };
